0bf4347503082158e96fa99ee6bae76caea8fb7e
souvik2805 <57298478+souvik2805@users.noreply.github.com>
2020-07-06 13:19:05 +0530

    Update and rename adfdsaf to isCircular.java

---
+++ LinkedList/isCircular.java
@@ -0,0 +1,21 @@
+public boolen isCircular(Node head){
+      Node temp = head;
+      if(head == null)
+        return null;
+	  
+	  while(temp != head){
+	  	temp = temp.next;
+	  }
+	  // This code create infinite loop for not Circular Linked List....
+	  
+	  while(temp != head && temp != null){
+	    temp = temp.next;
+	  }
+	  
+	  if(temp == head){
+	    return true;
+	  }
+	  else{
+	    return false;
+	  }
+}
