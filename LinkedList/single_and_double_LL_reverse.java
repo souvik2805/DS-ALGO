13125d0737ffea450c02cacc423585a5d2f8dfd8
souvik2805 <57298478+souvik2805@users.noreply.github.com>
2020-07-09 09:52:56 +0530

    Update and rename reverse.java to single_and_double_LL_reverse.java

--- LinkedList/single_and_double_LL_reverse.java
+++ LinkedList/single_and_double_LL_reverse.java
@@ -13,3 +13,23 @@ public Node reverse( Node head){
     return prev;
    
 }
+
+
+//  Double LL
+
+
+public static Node Reverse(Node head) {
+ Node prev = null;
+ Node curr = head;
+ Node next = head;
+ while(curr != null){
+   next = curr.next;
+   curr.next = prev;
+   curr.prev = next;
+
+   prev = curr;
+   curr = next;
+
+ }
+ return prev;
+}
