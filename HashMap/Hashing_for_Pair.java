aebf2f8281d75fd7c244f192a84a582b3681f3ba
souvik2805 <57298478+souvik2805@users.noreply.github.com>
2020-07-11 15:30:27 +0530

    Update Hashing_for_Pair.java

--- HashMap/Hashing_for_Pair.java
+++ HashMap/Hashing_for_Pair.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.util.Map.Entry;
 class Main{
     public static void main(String[] args){
-        
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         while(T-->0){
@@ -15,22 +14,73 @@ class Main{
             List.put(i,temp);
         }
          for(int i=0; i<n; i++){
-        
             int curr = List.get(i);
             int remain = k - curr;
-            List.replace(i,0);
-           
+            List.replace(i,0); 
             if(remain>0 && List.containsValue(remain)){
               flag = 1;
               break;
             }
             List.replace(i,curr);
-           
         }
-     
         System.out.println(flag);
-     
-      
     }
     }
 }
+
+// -----------------------------Optimize Solution_________________________>
+
+import java.util.*;
+import java.util.Map.Entry;
+class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0){
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        Map<Integer,Integer> List = new HashMap<>();
+        int flag = 0;
+        for(int i=0; i<n; i++){
+           int temp = sc.nextInt();
+            if(List.containsKey(temp)){
+                flag = 1;
+                sc.nextLine();
+                break;
+            }
+            else{
+                List.put((k-temp),1);
+            }
+           
+        }
+         
+     
+        System.out.println(flag);
+     
+        
+    }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
