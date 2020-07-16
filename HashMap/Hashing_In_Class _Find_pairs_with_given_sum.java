931783234c2b1f44c87df9703b924825c1c3dd7c
souvik2805 <57298478+souvik2805@users.noreply.github.com>
2020-07-16 09:07:50 +0530

    Update Hashing_In_Class _Find_pairs_with_given_sum.java

--- HashMap/Hashing_In_Class _Find_pairs_with_given_sum.java
+++ HashMap/Hashing_In_Class _Find_pairs_with_given_sum.java
@@ -52,3 +52,73 @@ class Main{
         return count;
     }
 }
+
+
+//--------------------------------------------------------------------------------------------------------------------------------->
+
+import java.util.*;
+import java.util.Map.Entry;
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException{
+        
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String []num = br.readLine().split(" ");
+    String [] arr = br.readLine().split(" ");
+    int n = Integer.parseInt(num[0]);
+    int k = Integer.parseInt(num[1]);
+    int arry[] = new int[n];
+   
+
+        Map<Integer,Integer> List = new HashMap<>();
+        int flag = 0;
+        for(int i=0; i<n; i++){
+           int temp = Integer.parseInt(arr[i]);
+           arry[i] = temp;
+            if(List.containsKey(temp)){
+                int value = List.get(temp);
+                value = value+1;
+                List.replace(temp,value);
+            }
+            else{
+              List.put((temp),1);
+            } 
+        }
+        System.out.println(count(List,arry,k));
+    
+    }
+
+    public static long count(Map<Integer,Integer> map,int [] arry, int k){
+        long count = 0;
+        for(int i=0; i<arry.length; i++){
+            int key = arry[i];
+            int value = map.getOrDefault(k-key,0);
+            count +=value;
+            if(k-arry[i] == arry[i]){
+                count--;
+            }
+        }
+        return count/2;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
