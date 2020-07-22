0da24ff772fd921cce6188d5638ba1c8a575e0cc
souvik2805 <57298478+souvik2805@users.noreply.github.com>
2020-07-22 20:53:14 +0530

    Create infix_postfix_prefix.java

---
+++ Stack/infix_postfix_prefix.java
@@ -0,0 +1,6 @@
+//*** INFIX POSTFIX & PREFIX
+//INFIX ORDER OF OPERATION
+1. Parenthese (){}[]
+2. Exponents (2^2^4) (RIGHT TO LEFT)
+3. Multiplication and Division (LEFT TO RIGHT)
+4. Addition and Subtraction (LEFT TO RIGHT)
