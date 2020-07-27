8e0e65527beac228e4c61c5acc88f91267717121
souvik2805 <57298478+souvik2805@users.noreply.github.com>
2020-07-27 13:08:15 +0530

    Update intro.java

--- Queue/intro.java
+++ Queue/intro.java
@@ -29,3 +29,46 @@ class Main{
         System.out.println(queue.peek());
     }
 }
+
+
+// Method - 2
+//PriorityQueue implement
+// PriorityQueue => Store element by Priority
+// --------------> Least element is Highest Priority
+// So after every add and remove operation on the queue the Least Element of the Queue get the hight priory so it come first.
+
+
+import java.util.*;
+class Main{
+    public static void print(Queue<Integer> queue){
+        Iterator itr = queue.iterator();
+        while(itr.hasNext()){
+            System.out.print(itr.next()+" ");
+        }
+
+        System.out.println();
+    }
+    // Map<Integer, Integer> map = new HashMap<>();
+    // Iterator itr = map.entrySet().iterator();
+    public static void main(String[] args){
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.add(1);
+        queue.add(11);
+        queue.add(3);
+        queue.add(4);
+          queue.add(10);
+        queue.add(12);
+         print(queue);
+        // Alternative of add in Queue is offer
+        queue.offer(20);
+        // Alternative of remove is poll
+        queue.poll();
+        // queue.remove();
+        print(queue);
+        // See first element of Queue
+        System.out.println(queue.peek());
+    }
+}
+
+
+
